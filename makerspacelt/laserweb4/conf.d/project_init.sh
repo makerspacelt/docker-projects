a6efaa7454914e377cf8471c7d1f59aca18409bf
project <project@docker>
2018-01-04 21:59:27 +0200

    initing project

--- makerspacelt/laserweb4/conf.d/project_init.sh
+++ makerspacelt/laserweb4/conf.d/project_init.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+set -e
+#set -x
+
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/src"
+
+echo "PROJECT ROOT: ${PROJECT_ROOT}"
+cd "${PROJECT_ROOT}"
+
 sleep 3
 
-npm install
+echo -e "\n## Getting submodules ... "
 
+git submodule init
+git submodule update
+
+
+echo -e "\n## Install dependencies ... "
+npm install
+npm run installdev
+
+echo -e "\n## Build webpack ... "
 npm run bundle-dev
 
